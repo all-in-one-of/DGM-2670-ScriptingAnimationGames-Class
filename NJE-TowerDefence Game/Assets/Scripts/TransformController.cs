d0126cfa9fafbe834fc9a7b8b93d9308050e869d
Nathan Ethington <10598429@my.uvu.edu>
2019-10-07 17:54:13 -0600

    Added some sample Scripts

---
+++ NJE-TowerDefence Game/Assets/Scripts/TransformController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class TransformController : MonoBehaviour
+{
+	public Create_Float speed ;
+	
+	private void Update()
+	{
+		var speedVector = new Vector3(speed.value, 0 , 0)*Time.deltaTime;
+		transform.Translate(speedVector);
+	}
+	
+}
