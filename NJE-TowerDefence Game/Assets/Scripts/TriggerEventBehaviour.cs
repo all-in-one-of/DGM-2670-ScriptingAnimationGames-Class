806566fd2c983e103cfffec9c9d0e3da8d924ca9
Nathan Ethington <10598429@my.uvu.edu>
2019-10-09 18:52:04 -0600

    Experimented with some code
    not much to wright about

--- NJE-TowerDefence Game/Assets/Scripts/TriggerEventBehaviour.cs
+++ NJE-TowerDefence Game/Assets/Scripts/TriggerEventBehaviour.cs
@@ -4,8 +4,9 @@ using UnityEngine.Events;
 public class TriggerEventBehaviour : MonoBehaviour
 {
 	public UnityEvent TriggerEnterEvent;
+	public UnityEvent TriggerExitEvent;
 	
-	private void OnTriggerEnter(Collider other)
+	private void OnTriggerEnter(Collider self)
 	{
 		TriggerEnterEvent.Invoke();
 	}
